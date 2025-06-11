b8d929f9f875a20b86082d34a16f9e66d4c2b2ea
D Astlin Dijo <97329667+Doctorstra@users.noreply.github.com>
2025-06-11 12:28:39 +0530

    Create ffmpeg_utils.py

---
+++ ffmpeg_utils.py
@@ -0,0 +1,11 @@
+# bot/utils/ffmpeg_utils.py
+import subprocess
+
+def merge_videos(input_files, output_path):
+    with open("inputs.txt", "w") as f:
+        for fpath in input_files:
+            f.write(f"file '{fpath}'\n")
+    subprocess.run(
+        ["ffmpeg", "-y", "-f", "concat", "-safe", "0", "-i", "inputs.txt", "-c", "copy", output_path],
+        check=True
+    )
