04693797f7b3aa7cd1c12a1f1d89b0b81c2bd4f9
D Astlin Dijo <97329667+Doctorstra@users.noreply.github.com>
2025-06-11 12:38:27 +0530

    Add files via upload

---
+++ configs.py
@@ -0,0 +1,7 @@
+import os
+
+API_ID = int(os.getenv("API_ID", 0))
+API_HASH = os.getenv("API_HASH", "")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+MONGO_URI = os.getenv("MONGO_URI", "")
+LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", 0))
